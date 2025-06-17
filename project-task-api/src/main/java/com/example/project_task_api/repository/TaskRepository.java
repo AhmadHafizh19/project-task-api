6ecbba026a53788902a2806c3aacf752e3801d69
Ahmad Hafizh Assaad <71488@fif.co.id>
2025-06-17 11:01:43 +0700

    :white_check_mark: Add feature/create-repository

--- project-task-api/src/main/java/com/example/project_task_api/repository/TaskRepository.java
+++ project-task-api/src/main/java/com/example/project_task_api/repository/TaskRepository.java
@@ -1,4 +1,10 @@
 package com.example.project_task_api.repository;
 
-public class TaskRepository {
+import com.example.project_task_api.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findByProjectId(Long projectId);
 }
